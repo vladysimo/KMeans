cdd80b4dd56b8538efb3df6ffe19fdc638f67c53
Andrei Vlad Postoaca <vlad.postoaca@gmail.com>
2018-05-21 15:23:13 +0300

    Adapted for PAT

--- cmeans.sh
+++ cmeans.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 $HADOOP_HOME/bin/hdfs dfs -rm -r /user/vlad/output_*
-$HADOOP_HOME/bin/hadoop jar /home/vlad/development/KMeans/out/artifacts/KMeans_jar/KMeans.jar kmeans.KMeans &> out_kmeans
-
-$HADOOP_HOME/bin/hdfs dfs -rm -r /user/vlad/output_*
 $HADOOP_HOME/bin/hadoop jar /home/vlad/development/KMeans/out/artifacts/CMeans_jar/KMeans.jar cmeans.CMeans &> out_cmeans
